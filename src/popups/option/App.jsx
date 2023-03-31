025c11366e1510cad1d4345ffd6b88e2531bd77b
spksoft <spkrsk.37@gmail.com>
2023-03-31 22:57:34 +0700

    feat: add option page chore: add antd

---
+++ src/popups/option/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { DatePicker } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ChatGPT for Chrome based browser</h1>
+      <div className="card">
+        <DatePicker />
+      </div>
+    </div>
+  );
+}
+
+export default App;
